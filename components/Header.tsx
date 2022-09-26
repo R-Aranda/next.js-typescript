cd9fb8e25606ef20c79d1e2f16c7dcd6ec4a8ea6
R-Aranda <rupertaranda@gmail.com>
2022-09-26 14:45:15 -0400

    social icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,61 @@
 import React from "react";
-
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 type Props = {};
 
 export default function Header({}: Props) {
-  return <div>Header</div>;
+  return (
+    <header>
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://github.com/R-Aranda"
+          className="cursor-pointer"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/rupert-aranda/"
+          className="cursor-pointer"
+        />
+        <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{
+            duration: 1,
+          }}
+        >
+          <SocialIcon
+            className="cursor-pointer"
+            network="email"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            Get In Touch
+          </p>
+        </motion.div>
+      </motion.div>
+    </header>
+  );
 }
